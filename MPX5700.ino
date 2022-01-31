325502fa48bfbaf3dde3d6f966c8092f3f31eb5b
enricoroselino <enricoroselino@gmail.com>
2022-01-31 18:12:03 +0700

    add mmhg

--- MPX5700.ino
+++ MPX5700.ino
@@ -15,21 +15,15 @@ void loop() {
 }
 
 void tekananSensor(){
-  long voltage;
-  int sensorRead, voltageMap,
-  VCC = 5000,
-  fullScale = 4700; //Sensor fullscale voltage 4700
-  float voltageMean, kpa, psi, atm, bar,
+  float sensorRead, voltageMean,voltage, kpa, psi, atm, bar, mmhg,
   offset = 0; //Adjust until the sensor output reaches 101,325 kPa or 14,69594878 psi or 1 atm
   voltage = 0.0;
   for(int i = 0; i < sampelData; i++){
-    sensorRead = (analogRead(A0) + 0.5) * (VCC / 1024);
-    //map the "ADC voltage" output from the ADC default reading by Vref (VCC at 1024) to sensor fullscale (4.7V at 963)
-    voltageMap = map(sensorRead, 0, VCC, 0, fullScale);
-    voltage += voltageMap;
+    sensorRead = (analogRead(A0) + 0.5) * (5.0 / 1024.0);
+    voltage += sensorRead;
     delay(10);
   }
-  voltageMean = ((voltage / sampelData) / 1000.0) + offset;
+  voltageMean = (voltage / (float)sampelData) + offset;
   kpa = ((voltageMean - 0.2) / 0.006429);
   if(kpa <= 0){
     kpa = 0.0;
@@ -37,6 +31,7 @@ void tekananSensor(){
   psi = kpa * 0.14503774;
   atm = kpa * 0.00986923;
   bar = kpa / 100.0;
+  mmhg= kpa * 7.501;
   printf("ADC Voltage = %f", voltageMean);
-  printf("\nTekanan = %f kPa atau %f psi atau %f atm atau %f bar\n", kpa, psi, atm, bar);
+  printf("\nTekanan = %f kPa atau %f psi atau %f atm atau %f bar atau %f mmhg\n", kpa, psi, atm, bar, mmhg);
 }
