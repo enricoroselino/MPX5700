5630b60d04979d4d30ae87cb2501c7d80850806b
enricoroselino <enricoroselino@gmail.com>
2022-02-05 04:13:46 +0700

    change note and delete map()

--- MPX5700.ino
+++ MPX5700.ino
@@ -19,6 +19,7 @@ void tekananSensor(){
   /*
    * Sensor full-scale = 4.7V
    * Max = 700 KPa
+   * (this mean if the sensor given 700KPa and 5VCC, it will only output 4.7V max ?)
    * Datasheet formula :
    * VS = 5V
    * Vout = VS * (0.0012858*P + 0.04)
@@ -28,9 +29,8 @@ void tekananSensor(){
    * Vout = (0.006429 * 101.325) + 0.2
    * Vout = 0.851418425
    */
-  int sensorRead,
-  fullScale = 4700;
-  float voltageMean, voltageMap, kpa, psi, atm, bar, mmhg,
+  int sensorRead;
+  float voltageMean, kpa, psi, atm, bar, mmhg,
   calValue = 0.851418425,
   offset = 0; //Adjust until the sensor output reaches 101,325 kPa
   sensorRead = 0;
@@ -38,14 +38,13 @@ void tekananSensor(){
   for(int i = 0; i < sampelData; i++){
     sensorRead += analogRead(pin);
   }
-  voltageMean = (sensorRead / (float)sampelData) * (5000.0 / 1024.0);
-  voltageMap = (map((int)round(voltageMean), 0, 5000, 0, fullScale) / 1000.0) + offset;
-  kpa = ((voltageMap - 0.2) / 0.006429);
+  voltageMean = ((sensorRead / (float)sampelData) * (5.0 / 1024.0)) + offset;
+  kpa = (voltageMean - 0.2) / 0.006429;
   psi = kpa * 0.14503774;
   atm = kpa * 0.00986923;
   bar = kpa / 100.0;
   mmhg= kpa * 7.501;
-  //printf("Change offset to = %f", abs(calValue - voltageMap)); //un-comment to calibrate the sensor to 101,325 kPa
-  printf("ADC Voltage = %f", voltageMap);
+  //printf("Change offset to = %f", abs(calValue - voltageMean)); //un-comment to calibrate the sensor to 101,325 kPa
+  printf("ADC Voltage = %f", voltageMean);
   printf("\nTekanan = %f kPa atau %f psi atau %f atm atau %f bar atau %f mmhg\n", kpa, psi, atm, bar, mmhg);
 }
